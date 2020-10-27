1b05240dde522521e7bf951ff4a1d084641987a9
Jaime Abbariao <jaimeabbariao@pop-os.localdomain>
2020-10-26 23:24:51 -0400

    Fixed some stuff

--- src/utils.js
+++ src/utils.js
@@ -6,7 +6,11 @@ import { ENV_YAML_FILE } from './constants';
 const getDatabaseURI = () => {
   try {
     const envFile = fs.readFileSync(ENV_YAML_FILE, 'utf-8');
-    const {database: { uri }} = yaml.safeLoad(envFile);
+    const {
+      database: { 
+        uri 
+      }
+    } = yaml.safeLoad(envFile);
 
     return uri;
   } catch (e) {
